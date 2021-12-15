af92caea9c64edb1fde43ab84b8ef98944a10598
IVC1-5 <M501248855@GMAIL.COM>
2021-12-15 14:33:52 +0300

    _
    Signed-off-by: IVC1-5 <M501248855@GMAIL.COM>

--- src/ru/mirea/task4/TestAuthor.java
+++ src/ru/mirea/task4/TestAuthor.java
@@ -2,11 +2,11 @@ package ru.mirea.task4;
 
 public class TestAuthor {
     public static void main(String [] args){
-        Author ob1 = new Author("shahboz", "aminov1232@gamil.com", 'M');
-        Author ob2 = new Author("bahrom", "bahrom1232@gamil.com", 'M');
-        Author ob3 = new Author("sorbon", "sorbon@gamil.com", 'M');
+        Author ob1 = new Author("Franko", "iiejy7f@gmail.com", 'M');
+        Author ob2 = new Author("Jime", "elf022@gmail.com", 'M');
+        Author ob3 = new Author("Jack", "werj911@gmail.com", 'M');
         System.out.println(ob1);
-        ob1.setName("bahriddin");
+        ob1.setName("Alucard");
         System.out.println(ob1);
     }
 }
